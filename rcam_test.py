eb8e418c325da599f3d008c958b9e733d96eb739
jay6943 <jhkim.etri@gmail.com>
2026-06-04 22:03:48 +0900

    home rpi

--- rcam_test.py
+++ rcam_test.py
@@ -4,6 +4,7 @@ from picamera2 import Picamera2
 cam = Picamera2()
 # cfg = cam.create_video_configuration(main={'size':(640,480)})
 # cfg = cam.create_video_configuration(main={'size':(1280,720)})
+# cfg = cam.create_video_configuration(main={'size':(1920,1080)})
 cfg = cam.create_video_configuration()
 cam.configure(cfg)
 cam.start()
