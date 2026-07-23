ea0f990c16a7c2a9a4716268b7c86e71b76ecdae
jay6943 <jhkim.etri@gmail.com>
2026-07-23 15:00:39 +0900

    office

--- cam_test.py
+++ cam_test.py
@@ -1,6 +1,25 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2592)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1944)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1600)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1200)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
 while True:
   ret, frame = cap.read()
